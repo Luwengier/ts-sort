cf321cccfb133bfcd35e7a908e14c40c9f17c04a
Luwengier <g217881111@gmail.com>
2022-08-07 15:58:21 +0800

    Add CharactersCollection class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter'
-import { NumbersCollection } from './NumberCollection'
+import { CharactersCollection } from './CharactersCollection'
+// import { NumbersCollection } from './NumberCollection'
 
-const NumberCollection = new NumbersCollection([1001, 7, -5, 0])
-const sorter = new Sorter(NumberCollection)
+// const numberCollection = new NumbersCollection([1001, 7, -5, 0])
+// const sorter = new Sorter(numberCollection)
+// sorter.sort()
+// console.log(numberCollection.data)
+
+const charactersCollection = new CharactersCollection('cDXaFv')
+const sorter = new Sorter(charactersCollection)
 sorter.sort()
-console.log(NumberCollection.data)
+console.log(charactersCollection.data)
